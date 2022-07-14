3ea4b02bf3dbfcdde3d10c388b676a67ee5c6e12
vaparr <70992682+vaparr@users.noreply.github.com>
2022-07-14 01:58:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyoverseerr",
-    version="0.1.28",
+    version="0.1.32",
     url="https://github.com/vaparr/pyoverseerr",
     author="Vaparr",
     author_email="Vaparr@vaparr.org",
